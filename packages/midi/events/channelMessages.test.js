43d2088724babe3bbd33ed7a46e58f995f8d5ac8
Matthew Kaney <matthew@mindofmatthew.com>
2019-10-03 17:28:58 -0400

    Stub out more MIDI tests

--- packages/midi/events/channelMessages.test.js
+++ packages/midi/events/channelMessages.test.js
@@ -1,4 +1,6 @@
-const { noteOn, noteOff } = require('./index.js');
+const { noteOn, noteOff } = require('./channelMessages.js');
+
+describe('noteOff()');
 
 describe('noteOn()', () => {
   describe('with data', () => {
@@ -8,9 +10,45 @@ describe('noteOn()', () => {
       expect(message).toHaveLength(3);
       expect(message[0]).toBe(144);
     });
+
+    test('has the correct channel', () => {
+      expect(noteOn(0, 60, 127)[0]).toBe(144);
+      expect(noteOn(5, 60, 127)[0]).toBe(149);
+      expect(noteOn(15, 60, 127)[0]).toBe(159);
+    });
+
+    test('has the correct pitch', () => {
+      expect(noteOn(0, 60, 127)[1]).toBe(60);
+      expect(noteOn(0, 0, 127)[1]).toBe(0);
+      expect(noteOn(0, 140, 127)[1]).toBe(140);
+    });
+
+    test('has the correct velocity (if specified)', () => {
+      expect(noteOn(0, 60, 0)[2]).toBe(0);
+      expect(noteOn(0, 60, 127)[2]).toBe(127);
+      expect(noteOn(15, 60)[2]).toBe(127);
+    });
   });
 
   describe('with a callback', () => {
     test.todo('test');
   });
 });
+
+describe('noteOn()');
+
+describe('keyPressure()');
+
+describe('controlChange()');
+
+describe('programChange()');
+
+describe('channelPressure()');
+
+describe('pitchBend()');
+
+describe('allSoundOff()');
+
+describe('resetControllers()');
+
+describe('localControl()');
