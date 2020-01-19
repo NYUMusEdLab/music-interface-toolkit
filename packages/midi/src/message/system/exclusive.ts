bccb298b2504cf2153ec1d3aad695d0bed43b646
Matthew <matthew@mindofmatthew.com>
2020-01-19 01:21:04 -0500

    Add midi system exclusive handler

--- packages/midi/src/message/system/exclusive.ts
+++ packages/midi/src/message/system/exclusive.ts
@@ -1,8 +1,29 @@
-import { MidiData } from '../../types';
+import { MidiData, MidiMessage } from '../../types';
 import { SYSTEM_EXCLUSIVE, END_OF_EXCLUSIVE } from '../../data/statusBytes';
 
-function systemExclusive(manufacturer: number[], data: MidiData) {
+export interface SystemExclusiveMessage extends MidiMessage {
+  manufacturer: number[];
+  systemExclusiveData: MidiData;
+}
+
+export function systemExclusive(manufacturer: number[], data: MidiData) {
   return [SYSTEM_EXCLUSIVE, ...manufacturer, ...data, END_OF_EXCLUSIVE];
 }
 
-//function onSystemExclusive(fn: ())
+export function onSystemExclusive(fn: (m: SystemExclusiveMessage) => void) {
+  return (m: MidiMessage) => {
+    let {
+      data: [status, ...data]
+    } = m;
+
+    if (status === SYSTEM_EXCLUSIVE) {
+      let manufacturer = data.slice(0, data[0] !== 0 ? 1 : 3);
+      let systemExclusiveData = data.slice(
+        manufacturer.length,
+        data.indexOf(END_OF_EXCLUSIVE)
+      );
+
+      fn({ ...m, manufacturer, systemExclusiveData });
+    }
+  };
+}
