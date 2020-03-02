c38602d5fd77a2301310ed8367b107af62f8654c
Matthew <matthew@mindofmatthew.com>
2020-03-02 15:45:47 -0500

    Work on cleaning up build system

--- packages/piano-ui/rollup.config.js
+++ packages/piano-ui/rollup.config.js
@@ -1,10 +1,12 @@
 import resolve from 'rollup-plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
+import ts from '@wessberg/rollup-plugin-ts';
 import babel from 'rollup-plugin-babel';
 
 export default {
   external: ['react'],
   plugins: [
+    ts(),
     resolve(),
     postcss({
       extensions: ['.css']
@@ -15,7 +17,7 @@ export default {
     })
   ],
   input: {
-    'piano/index': 'src/index.jsx'
+    'piano/index': 'src/index.tsx'
   },
   output: {
     dir: '.',
